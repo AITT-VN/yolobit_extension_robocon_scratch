19f3639a649fe678e8234251f9264ae0366cac24
truongphat230799 <taphat1753@gmail.com>
2023-07-23 14:02:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -81,11 +81,11 @@ class Scratch3YoloBitRobocon {
                     arguments: {
                         SPEED_LEFT: {
                             type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: 50
+                            defaultValue: '50'
                         },
                         SPEED_RIGHT: {
                             type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: 50
+                            defaultValue: '50'
                         },
                         TIME_DELAY: {
                             type: Scratch.ArgumentType.NUMBER,
@@ -112,11 +112,11 @@ class Scratch3YoloBitRobocon {
                     arguments: {
                         SPEED_LEFT: {
                             type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: 50
+                            defaultValue: '50'
                         },
                         SPEED_RIGHT: {
                             type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: 50
+                            defaultValue: '50'
                         },
                         TIME_DELAY: {
                             type: Scratch.ArgumentType.NUMBER,
